fc80c0211e6cf1e2aedfccdc9d9ac3e583ef2fb0
Zaneb1991 <zaneb123@hotmail.co.uk>
2024-07-14 13:51:55 +0100

    Update One_Page_Website.js

--- One_Page_Website/JS/One_Page_Website.js
+++ One_Page_Website/JS/One_Page_Website.js
@@ -1,36 +1,35 @@
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-}
-  
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-var slideIndex = 1;
-showSlides(slideIndex);
-  
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  slideIndex += n;
+  if (slideIndex > document.getElementsByClassName("mySlides").length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = document.getElementsByClassName("mySlides").length;
+  }
+  showSlides();
 }
-  
+
 function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-  
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+  slideIndex = n;
+  showSlides();
 }
